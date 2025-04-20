870fb3d35d5a202e352ec3593666a537f98ac5dd
sergFibih <sergfibih@ya.ru>
2025-04-20 15:40:24 +1000

    commit #1

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -1,23 +1,29 @@
 package ru.hogwarts.school.model;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
 
 public class Student {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String name;
     private int age;
+    private String name;
 
     public Student(long id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
+
+    public Student(){}
 
     public long getId() {
         return id;
