d9c0f9c2e21ab75fb623a589bbd2a158eeab2fa7
sergFibih <sergfibih@ya.ru>
2025-05-20 21:51:44 +1000

    доработка для слияния

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -22,7 +22,12 @@ public class Faculty {
 
 
     public Faculty(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
 
+    public Faculty(Long id, String name, String color) {
+        this.id = id;
         this.name = name;
         this.color = color;
     }
